1225f973f92b37bb5832059d23c8555ceea74026
mvgreen <mvgreen342@gmail.com>
2020-05-06 19:57:00 +0400

    Update generate.py

--- generate.py
+++ generate.py
@@ -1,4 +1,3 @@
-  
 from PIL import Image
 import torch
 from torchvision import transforms
@@ -21,23 +20,22 @@ class Converter_eval:
 
 class Converter_other:
 
-	def __init__(self, input_dir):
-		self.input_dir = input_dir
-		self.transform = transforms.Compose([transforms.ToTensor()])
-
+    def __init__(self, input_dir):
+        self.input_dir = input_dir
+        self.transform = transforms.Compose([transforms.ToTensor()])
+	
     def Test(self, model, output_dir, logfile=None):
-		file_list = sorted(os.listdir(self.input_dir))
-		for i in (len(file_list) - 1):
-			in_filename, in_ext = re.split('\.', file_list[i])
-			out_filename = in_filename + 'a' + in_ext
-			
-      im1 = Image.open(self.input_dir + '/' + file_list[i])
-      im2 = Image.open(self.input_dir + '/' + file_list[i+1])
+        file_list = sorted(os.listdir(self.input_dir))
+        for i in (len(file_list) - 1):
+            in_filename, in_ext = re.split('\.', file_list[i])
+            out_filename = in_filename + 'a' + in_ext
+            
+            im1 = Image.open(self.input_dir + '/' + file_list[i])
+            im2 = Image.open(self.input_dir + '/' + file_list[i+1])
 			img1 = to_variable(self.transform(im1).unsqueeze(0))
 			img2 = to_variable(self.transform(im2).unsqueeze(0))
-			
-      frame_out = model(img1, img2)
-			
-			imwrite(frame_out, output_dir + '/' + out_filename)
-      im1.close()
-      im2.close()
+            
+            frame_out = model(img1, img2)
+            imwrite(frame_out, output_dir + '/' + out_filename)
+            im1.close()
+            im2.close()
