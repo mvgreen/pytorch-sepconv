d90e3082e69d53bcf8b0a1ceec373189c99fb9bd
mvgreen <mvgreen342@gmail.com>
2020-05-06 17:34:58 +0400

    Update TestModule.py

--- TestModule.py
+++ TestModule.py
@@ -5,7 +5,7 @@ from math import log10
 from torchvision.utils import save_image as imwrite
 from torch.autograd import Variable
 import os
-import re
+
 
 def to_variable(x):
     if torch.cuda.is_available():
@@ -13,28 +13,43 @@ def to_variable(x):
     return Variable(x)
 
 
-class Converter_eval:
+class Middlebury_eval:
     def __init__(self, input_dir):
-        self.im_list # записать список имен папок с данными
+        self.im_list = ['Army', 'Backyard', 'Basketball', 'Dumptruck', 'Evergreen', 'Grove', 'Mequon', 'Schefflera', 'Teddy', 'Urban', 'Wooden', 'Yosemite']
 
 
-class Converter_other:
-	def __init__(self, input_dir):
-		self.input_dir = input_dir
-		self.transform = transforms.Compose([transforms.ToTensor()])
+class Middlebury_other:
+    def __init__(self, input_dir, gt_dir):
+        self.im_list = ['Beanbags', 'Dimetrodon', 'DogDance', 'Grove2', 'Grove3', 'Hydrangea', 'MiniCooper', 'RubberWhale', 'Urban2', 'Urban3', 'Venus', 'Walking']
+        self.transform = transforms.Compose([transforms.ToTensor()])
 
-	def Test(self, model, output_dir, logfile=None):
-		file_list = sorted(os.listdir(self.input_dir))
-		for i in range(0, len(file_list) - 1):
-			in_filename, in_ext = re.split('\.', file_list[i])
-			out_filename = in_filename + 'a' + in_ext
-			
-			im1 = Image.open(self.input_dir + '/' + file_list[i])
-			im2 = Image.open(self.input_dir + '/' + file_list[i+1])
-			img1 = to_variable(self.transform(im1).unsqueeze(0))
-			img2 = to_variable(self.transform(im2).unsqueeze(0))
-			
-			frame_out = model(img1, img2)
-			imwrite(frame_out, output_dir + '/' + out_filename)
-			im1.close()
-			im2.close()
+        self.input0_list = []
+        self.input1_list = []
+        self.gt_list = []
+        for item in self.im_list:
+            self.input0_list.append(to_variable(self.transform(Image.open(input_dir + '/' + item + '/frame10.png')).unsqueeze(0)))
+            self.input1_list.append(to_variable(self.transform(Image.open(input_dir + '/' + item + '/frame11.png')).unsqueeze(0)))
+            self.gt_list.append(to_variable(self.transform(Image.open(gt_dir + '/' + item + '/frame10i11.png')).unsqueeze(0)))
+
+    def Test(self, model, output_dir, logfile=None, output_name='output.png'):
+        av_psnr = 0
+        if logfile is not None:
+            logfile.write('{:<7s}{:<3d}'.format('Epoch: ', model.epoch.item()) + '\n')
+        for idx in range(len(self.im_list)):
+            if not os.path.exists(output_dir + '/' + self.im_list[idx]):
+                os.makedirs(output_dir + '/' + self.im_list[idx])
+            frame_out = model(self.input0_list[idx], self.input1_list[idx])
+            gt = self.gt_list[idx]
+            psnr = -10 * log10(torch.mean((gt - frame_out) * (gt - frame_out)).item())
+            av_psnr += psnr
+            imwrite(frame_out, output_dir + '/' + self.im_list[idx] + '/' + output_name, range=(0, 1))
+            msg = '{:<15s}{:<20.16f}'.format(self.im_list[idx] + ': ', psnr) + '\n'
+            print(msg, end='')
+            if logfile is not None:
+                logfile.write(msg)
+        av_psnr /= len(self.im_list)
+        msg = '{:<15s}{:<20.16f}'.format('Average: ', av_psnr) + '\n'
+        print(msg, end='')
+        if logfile is not None:
+            logfile.write(msg)
+            
